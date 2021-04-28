5e45a4df8724da952a30519b32f84180a4e1abd6
antoniocordeiro1 <cordeiro.antonio97@hotmail.com>
2021-04-28 02:00:47 +0100

    User landing page with header styled (still missing half-border-bottom)

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -3,40 +3,13 @@ import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
-import SideDrawer from "./SideDrawer";
-import Backdrop from "../UIElements/Backdrop";
 import "./MainNavigation.css";
 
-const MainNavigation = (props) => {
-  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-
-  const openDrawerHandler = () => {
-    setDrawerIsOpen(true);
-  };
-
-  const closeDrawerHandler = () => {
-    setDrawerIsOpen(false);
-  };
+const MainNavigation = () => {
 
   return (
     <React.Fragment>
-      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
-      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
-
       <MainHeader>
-        <button
-          className="main-navigation__menu-btn"
-          onClick={openDrawerHandler}
-        >
-          <span />
-          <span />
-          <span />
-        </button>
-
         <nav className="main-navigation__header-nav">
           <NavLinks />
         </nav>
--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 import { AuthContext } from "../../context/auth-context";
+import logo from "../../assets/img/logo-bacle.png";
 import "./NavLinks.css";
 
 const NavLinks = (props) => {
@@ -10,11 +11,11 @@ const NavLinks = (props) => {
 
   return (
     <ul className="nav-links">
-      <div className="nav-links__div">
+      <div className="nav-link__side-divs">
         <li>
-          <NavLink to="/sobre" exact>
-            Sobre Nós
-          </NavLink>
+            <NavLink to="/sobre" exact>
+              Sobre Nós
+            </NavLink>
         </li>
         <li>
           <NavLink to="/company" exact>
@@ -22,45 +23,44 @@ const NavLinks = (props) => {
           </NavLink>
         </li>
       </div>
-      <div className="nav-links__center">
         <li>
-          <h1 className="main-navigation__title">
-            <Link to="/">BACLE</Link>
-          </h1>
+            <div className="nav-logo__div">
+              <Link to="/" className="nav-logo">
+                <img src={logo} alt="Bacle Jobs" className="nav-logo__img" />
+              </Link>
+            </div>
         </li>
-      </div>
-
-      <div className="nav-links__div">
         {auth.isLoggedIn && (
           <li>
             <NavLink to="/user/u1/places">MY ACCOUNT</NavLink>
           </li>
         )}
 
-        {!auth.isLoggedIn && (
-          <li>
-            <NavLink to="/user/auth" exact>
-              INICIAR SESSÃO
-            </NavLink>
-          </li>
-        )}
-        {!auth.isLoggedIn && (
-          <li>
-            <NavLink
-              to="/user/register"
-              exact
-              className="landing-button_account"
-            >
-              CRIAR CONTA
-            </NavLink>
-          </li>
-        )}
+        <div className="nav-link__side-divs">
+          {!auth.isLoggedIn && (
+            <li>
+              <NavLink to="/user/auth" exact>
+                Iniciar Sessão
+              </NavLink>
+            </li>
+          )}
+          {!auth.isLoggedIn && (
+            <li>
+              <NavLink
+                to="/user/register"
+                exact
+                className="landing-button_account"
+              >
+                <b>Criar Conta</b>
+              </NavLink>
+            </li>
+          )}
+        </div>
         {auth.isLoggedIn && (
           <li>
             <button onClick={auth.logout}>LOGOUT</button>
           </li>
         )}
-      </div>
     </ul>
   );
 };
